b9194f07e344fc68e8ee6fcc3d3d9957d069ee95
Juan Sáez García <contacto@juamber.com>
2021-10-27 14:54:23 +0200

    CalableStattement

--- 011.ExampleCallableStatement/src/examplecallablestatement/ExampleCallableStatement.java
+++ 011.ExampleCallableStatement/src/examplecallablestatement/ExampleCallableStatement.java
@@ -10,8 +10,10 @@ public class ExampleCallableStatement {
             String url = "jdbc:hsqldb:.\\lib\\database\\db\\db";
             Connection con = DriverManager.getConnection(url);
             Statement stt = con.createStatement();
-            String sql = "DROP PROCEDURE changeOffice IF EXISTS";
-            stt.executeUpdate(sql);
+            CallableStatement call;
+            String sql;
+            String sqlselect = "SELECT * FROM departments";
+            
             sql = "DROP PROCEDURE changeOffice1 IF EXISTS";
             stt.executeUpdate(sql);
             sql = "DROP PROCEDURE changeOffice2 IF EXISTS";
@@ -19,56 +21,50 @@ public class ExampleCallableStatement {
             sql = "DROP PROCEDURE changeOffice3 IF EXISTS";
             stt.executeUpdate(sql);
             
-            /*sql = "CREATE PROCEDURE changeOffice()" +
-                    "    modifies sql data " +
-                    "    UPDATE departments SET office = 'OFFICE'" +
-                    ";";
             
-            stt.executeUpdate(sql);
-            
-            sql = "SELECT * FROM departments";
-            
-            JDBChelper.showResultSet(stt.executeQuery(sql));
-            
-            CallableStatement call = con.prepareCall("CALL changeOffice()");
-            call.execute();
-            JDBChelper.showResultSet(stt.executeQuery(sql));*/
-            
-            sql = "CREATE PROCEDURE changeOffice1(offi VARCHAR(20))" +
+            System.out.println("ChangeOffice1");
+            sql =   "CREATE PROCEDURE changeOffice1(offi VARCHAR(20))" +
                     "    modifies sql data " +
                     "    UPDATE departments SET office = offi;";
             
             stt.executeUpdate(sql);
-            CallableStatement call = con.prepareCall("CALL changeOffice1(?)");
-            String variable = "PROCEDURE1";
-            call.setString(1,variable);
+            call = con.prepareCall("CALL changeOffice1(?)");
+            call.setString(1,"PROCEDURE1");
             call.execute();
-            sql = "SELECT * FROM departments";
-            JDBChelper.showResultSet(stt.executeQuery(sql));
+            JDBChelper.showResultSet(stt.executeQuery(sqlselect));
+            System.out.println("\n\n");
             
-            /*sql = "CREATE PROCEDURE changeOffice2(offi VARCHAR(20))" +
+            
+            System.out.println("ChangeOffice2");
+            sql =   "CREATE PROCEDURE changeOffice2(offi VARCHAR(20))" +
                     "    modifies sql data " +
-                    "    UPDATE departments SET office = offi WHERE name = 'INFORMÁTICA';";
+                    "    UPDATE departments SET office = offi WHERE name = 'INFORMµTICA';";
             
             stt.executeUpdate(sql);
             call = con.prepareCall("CALL changeOffice2(?)");
-            call.setString('PROCEDURE2');
+            call.setString(1,"PROCEDURE2");
             call.execute();
-            JDBChelper.showResultSet(stt.executeQuery(sql));
+            JDBChelper.showResultSet(stt.executeQuery(sqlselect));
+            System.out.println("\n\n");
             
-            stt = con.createStatement();
-            sql = "CREATE PROCEDURE changeOffice3(pattern VARCHAR(20), offi VARCHAR(20))" +
+            
+            System.out.println("ChangeOffice3");
+            sql =   "CREATE PROCEDURE changeOffice3(pattern VARCHAR(20), offi VARCHAR(20))" +
                     "    modifies sql data" +
                     "    UPDATE departments SET office = offi WHERE name LIKE pattern;";
             
+            stt.executeUpdate(sql);
             call = con.prepareCall("CALL changeOffice3(?,?)");
-            call.setString('%O');
-            call.setString('PROCEDURE3');
+            call.setString(1,"%O");
+            call.setString(2,"PROCEDURE3");
             call.execute();
-            JDBChelper.showResultSet(stt.executeQuery(sql));*/
+            JDBChelper.showResultSet(stt.executeQuery(sqlselect));
+            System.out.println("\n\n");
             
             
-            stt = con.createStatement();
+            call.close();
+            stt.close();
+            con.close();
         } catch (SQLException ex) {
             System.out.println("ERROR: "+ex.getMessage());
         }
