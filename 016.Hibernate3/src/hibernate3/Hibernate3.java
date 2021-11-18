5d45516429e8278472346ac12c8f99c7f8bfd2b8
Juan Sáez García <contacto@juamber.com>
2021-11-18 09:44:47 +0100

    .

--- 016.Hibernate3/src/hibernate3/Hibernate3.java
+++ 016.Hibernate3/src/hibernate3/Hibernate3.java
@@ -1,4 +1,3 @@
-
 package hibernate3;
 
 import org.hibernate.Session;
@@ -9,17 +8,47 @@ import pojos.Departamentos;
 public class Hibernate3 {
 
     public static void main(String[] args) {
-        SessionFactory sessionFactory = NewHibernateUtil.getSessionFactory();
-        Session sesion = sessionFactory.openSession();
-        Transaction tx = sesion.beginTransaction();
-        Departamentos dep = new Departamentos (); //dep is transient
-        dep.setDeptNo(35);
-        dep.setDnombre ("Nuevo dep");
-        dep.setLoc ("locapto");
-        sesion.save (dep); //now dep becomes persistent
-        tx.commit();
-        sesion.close();
+        //SessionFactory sessionFactory = NewHibernateUtil.getSessionFactory();
+        //Session sesion = sessionFactory.openSession();
+        //Transaction tx = sesion.beginTransaction();
+        //Departamentos dep = new Departamentos (); //dep is transient
+        //dep.setDeptNo(35);
+        //dep.setDnombre ("Nuevo dep");
+        //dep.setLoc ("locapto");
+        //sesion.save (dep); //now dep becomes persistent
+        //tx.commit();
+        //sesion.close();
         //after closing sesion, dep is detached
+
+        Transaction tx - sesion.beginTransaction();
+        Departamentos dep = new Departamentos(); //dop Ls transient
+
+        dep.setDept.No((byte) 36);
+
+        dep.setDnombre("Nuevo dep");
+        dep.setLoc("locdpto");
+
+        Empleados emp = new Fmpleados(17) //emp is transient
+        emp.setEmpNo(117);
+        emp.setApellido("EZE");
+        emp.setDirt(7369);
+        emp.setSalario(float 1500):
+        emp.setComision(float 191);
+
+        emp.setDepar Laserlos des);
+
+        sesion.save(dep);
+        senion.save(emp); //nov trp becomes perszezent
+
+        Ex.commit():
+
+sesion.close
+        
+    
+
+    
+
+: ?
     }
     
 }
