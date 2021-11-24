f95eec633d005145f64993b7f00e38a45f9e6220
Juan Sáez García <contacto@juamber.com>
2021-11-24 13:44:05 +0100

    .

--- 016.Act4_2/src/act4_2/Act4_2.java
+++ 016.Act4_2/src/act4_2/Act4_2.java
@@ -1,10 +1,19 @@
 package act4_2;
 
 import hibernate_resources.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Date;
 import org.hibernate.*;
 import pojos.*;
 import java.util.Scanner;
-import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class Act4_2 {
@@ -12,11 +21,11 @@ public class Act4_2 {
     private static Scanner sc = new Scanner(System.in);
     private static SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         menu();
     }
 
-    public static void menu() {
+    public static void menu() throws ParseException {
         boolean key = true;
 
         while (key) {
@@ -35,8 +44,11 @@ public class Act4_2 {
                     + "11. Quit\n");
             int op = sc.nextInt();
             int id;
-            int deptNum;
+            int deptNum,salary,per,years;
             String name,office;
+            String namet,surnamet,emailt,d;
+            Date startDatet;
+            
             switch (op) {
                 case 1: 
                     System.out.println("ID (Department): ");
@@ -62,12 +74,74 @@ public class Act4_2 {
                     office = sc.nextLine();
                     createDepartment(deptNum,name,office); 
                 break;
-                case 5: createTeacherAndDepartment(); break;
-                case 6: createTeacherInExistingDepartment(); break;
-                case 7: deleteTeacher(); break;
-                case 8: deleteDepartment(); break;
-                case 9: setSalaryOfDepartment(); break;
-                case 10: riseSalaryOfDepartmentSeniors(); break;
+                case 5: 
+                    System.out.println("deptNum (Department): ");
+                    deptNum = sc.nextInt();sc.nextLine();
+                    System.out.println("name (Department): ");
+                    name = sc.nextLine();
+                    System.out.println("office (Department): ");
+                    office = sc.nextLine();
+                    
+                    System.out.println("id (Teacher): ");
+                    id = sc.nextInt();sc.nextLine();
+                    System.out.println("name (Teacher): ");
+                    namet = sc.nextLine();
+                    System.out.println("surname (Teacher): ");
+                    surnamet = sc.nextLine();
+                    System.out.println("email (Teacher): ");
+                    emailt = sc.nextLine();
+                    System.out.println("start date (dd/MM/yyyy) (Teacher): ");
+                    d = sc.nextLine();
+                    startDatet = new SimpleDateFormat("dd/MM/yyyy").parse(d);  
+                    System.out.println("salary (Teacher): ");
+                    salary = sc.nextInt();
+                    createTeacherAndDepartment(deptNum,name,office,id,namet,surnamet,emailt,startDatet,salary); 
+                break;
+                case 6: 
+                    System.out.println("deptNum (Department): ");
+                    deptNum = sc.nextInt();sc.nextLine();
+                    System.out.println("id (Teacher): ");
+                    id = sc.nextInt();sc.nextLine();
+                    System.out.println("name (Teacher): ");
+                    namet = sc.nextLine();
+                    System.out.println("surname (Teacher): ");
+                    surnamet = sc.nextLine();
+                    System.out.println("email (Teacher): ");
+                    emailt = sc.nextLine();
+                    System.out.println("start date (dd/MM/yyyy) (Teacher): ");
+                    d = sc.nextLine();
+                    startDatet = new SimpleDateFormat("dd/MM/yyyy").parse(d);  
+                    System.out.println("salary (Teacher): ");
+                    salary = sc.nextInt();
+                    
+                    createTeacherInExistingDepartment(deptNum,id,namet,surnamet,emailt,startDatet,salary);
+                    break;
+                case 7: 
+                    System.out.println("id (Teacher): ");
+                    id = sc.nextInt();sc.nextLine();
+                    deleteTeacher(id); 
+                break;
+                case 8: 
+                    System.out.println("deptNum (Department): ");
+                    deptNum = sc.nextInt();sc.nextLine();
+                    deleteDepartment(deptNum); 
+                break;
+                case 9: 
+                    System.out.println("deptNum (Department): ");
+                    deptNum = sc.nextInt();sc.nextLine();
+                    System.out.println("salary (Of all teachers of the Department "+deptNum+"): ");
+                    salary = sc.nextInt();sc.nextLine();
+                    setSalaryOfDepartment(deptNum,salary); 
+                break;
+                case 10: 
+                    System.out.println("deptNum (Department): ");
+                    deptNum = sc.nextInt();sc.nextLine();
+                    System.out.println("Percentage to raise salary (Of Seniors teachers of the Department "+deptNum+"): ");
+                    per = sc.nextInt();sc.nextLine();
+                    System.out.println("Years to be considered Senior: ");
+                    years = sc.nextInt();sc.nextLine();
+                    riseSalaryOfDepartmentSeniors(deptNum,per,years); 
+                break;
                 case 11: 
                     key = false; 
                     sessionFactory.close(); 
@@ -119,23 +193,135 @@ public class Act4_2 {
         System.out.println("Department Created");
         sesion.close();
     }
-    public static void createTeacherAndDepartment() {
+    public static void createTeacherAndDepartment(int deptNum,String name,String office,int id,String namet, String surnamet, String emailt, Date startDatet, Integer salary) {
         System.out.println("-----createTeacherAndDepartment-----");
+        
+        Session sesion = sessionFactory.openSession();
+        Transaction tx;
+        
+        tx = sesion.beginTransaction();
+        Departments department = new Departments(); 
+        department.setDeptNum(deptNum);
+        department.setName(name);
+        department.setOffice(office);
+        sesion.save(department);
+        tx.commit();
+        System.out.println("Department Created");
+        
+        tx = sesion.beginTransaction();
+        Teachers teacher = new Teachers(); 
+        teacher.setId(id);
+        teacher.setName(namet);
+        teacher.setSurname(surnamet);
+        teacher.setEmail(emailt);
+        teacher.setStartDate(startDatet);
+        teacher.setSalary(salary);
+        teacher.setDepartments(department);
+        sesion.save(teacher);
+        tx.commit();
+        System.out.println("Teacher Created");
+        sesion.close();
     }
-    public static void createTeacherInExistingDepartment() {
+    public static void createTeacherInExistingDepartment(int deptNum,int id,String namet, String surnamet, String emailt, Date startDatet, Integer salary) {
         System.out.println("-----createTeacherInExistingDepartment-----");
+        
+        Session sesion = sessionFactory.openSession();
+        Transaction tx = sesion.beginTransaction();
+        Teachers teacher = new Teachers(); 
+        teacher.setId(id);
+        teacher.setName(namet);
+        teacher.setSurname(surnamet);
+        teacher.setEmail(emailt);
+        teacher.setStartDate(startDatet);
+        teacher.setSalary(salary);
+        teacher.setDepartments((Departments) sesion.get(Departments.class, deptNum));
+        sesion.save(teacher);
+        tx.commit();
+        System.out.println("Teacher Created");
+        sesion.close();
     }
-    public static void deleteTeacher() {
+    public static void deleteTeacher(int id) {
         System.out.println("-----deleteTeacher-----");
+        Session sesion = sessionFactory.openSession();
+        Transaction tx = sesion.beginTransaction();
+        sesion.delete(sesion.get(Teachers.class, id));
+        tx.commit();
+        System.out.println("Teacher Deleted");
+        sesion.close();
     }
-    public static void deleteDepartment() {
+    public static void deleteDepartment(int deptNum) {
         System.out.println("-----deleteDepartment-----");
+        Session sesion = sessionFactory.openSession();
+        Transaction tx = sesion.beginTransaction();
+        Departments department = (Departments) sesion.get(Departments.class, deptNum);
+        
+        if(department.getTeacherses().size()>0){
+            System.out.println(department.getName()+" has associate professors. Do you want to delete them together with the department? (Y/N)");
+                String res = sc.nextLine();
+                if(res.toUpperCase().equals("Y")){
+                    department.getTeacherses().forEach(teacherObject -> {
+                        Teachers teacher = (Teachers) teacherObject;
+                        System.out.println("Deleting Teacher '"+ teacher.getName() + "'");
+                        sesion.delete(teacher);
+                    });
+                    System.out.println("Deleting Department " + department.getName());
+                    sesion.delete(department);
+                    tx.commit();
+
+                }else if(res.toUpperCase().equals("N")){
+                    System.out.println("No data has been deleted");
+                }else{
+                    deleteDepartment(deptNum);
+                }
+        }else{
+            sesion.delete(department);
+            tx.commit();
+        }
+        sesion.close();
     }
-    public static void setSalaryOfDepartment() {
+    public static void setSalaryOfDepartment(int deptNum, int salary) {
         System.out.println("-----setSalaryOfDepartment-----");
+        
+        Session sesion = sessionFactory.openSession();
+        Transaction tx = sesion.beginTransaction();
+        Departments department = (Departments) sesion.get(Departments.class, deptNum);
+        
+        if(department.getTeacherses().size()>0){
+            department.getTeacherses().forEach(teacherObject -> {
+                Teachers teacher = (Teachers) teacherObject;
+                System.out.println("Updating Salary Of Teacher '"+ teacher.getName() + "'");
+                sesion.delete(teacher);
+                teacher.setSalary(salary);
+            });
+            tx.commit();
+        }else{
+            System.out.println(department.getName()+" does not have teachers");
+        }
     }
-    public static void riseSalaryOfDepartmentSeniors() {
+    public static void riseSalaryOfDepartmentSeniors(int deptNum, int per, int years) throws ParseException{
         System.out.println("-----riseSalaryOfDepartmentSeniors-----");
+        
+        Session sesion = sessionFactory.openSession();
+        Transaction tx = sesion.beginTransaction();
+        Departments department = (Departments) sesion.get(Departments.class, deptNum);
+        
+        if(department.getTeacherses().size()>0){
+            department.getTeacherses().forEach(teacherObject -> {
+                Teachers teacher = (Teachers) teacherObject;
+                
+                Date start_date = teacher.getStartDate();
+                LocalDate date = start_date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                Clock cl = Clock.systemUTC();
+                LocalDate today = LocalDate.now(cl);    
+                
+                Period p = Period.between();
+                
+               
+            });
+            tx.commit();
+        }else{
+            System.out.println(department.getName()+" does not have teachers");
+        }
     }
     
 }
