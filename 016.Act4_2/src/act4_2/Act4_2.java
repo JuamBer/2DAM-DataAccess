c6d130b3447c05fbde73c1207746d8c9c04970a5
Juan Sáez García <contacto@juamber.com>
2021-11-25 18:13:17 +0100

    act42

--- 016.Act4_2/src/act4_2/Act4_2.java
+++ 016.Act4_2/src/act4_2/Act4_2.java
@@ -6,6 +6,7 @@ import pojos.*;
 import java.util.Scanner;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.InputMismatchException;
 
 public class Act4_2 {
 
@@ -256,13 +257,11 @@ public class Act4_2 {
             System.out.println("there is no department with the id " + deptNum);
         }
     }
-    
-    
-    
+
     public static void menu() throws Exception {
         boolean key = true;
 
-        while (key) {
+        do{
             System.out.println("\n\n");
             System.out.println("\nChoose an option:\n"
                     + "1. Show a department by ID\n"
@@ -276,123 +275,141 @@ public class Act4_2 {
                     + "9. Set salary of whole department\n"
                     + "10. Rise salary for seniors of department\n"
                     + "11. Quit\n");
-            int op = sc.nextInt();
-            int id;
-            int deptNum, salary, per, years;
-            String name, office;
-            String namet, surnamet, emailt, d;
-            Date startDatet;
 
-            switch (op) {
-                case 1:
-                    System.out.println("ID (Department): ");
-                    id = sc.nextInt();
-                    showDepartment(id);
-                    break;
-                case 2:
-                    System.out.println("ID (Teacher): ");
-                    id = sc.nextInt();
-                    showTeacher(id);
-                    break;
-                case 3:
-                    System.out.println("ID (Department): ");
-                    id = sc.nextInt();
-                    showTeachersInDepartment(id);
-                    break;
-                case 4:
-                    System.out.println("deptNum (Department): ");
-                    deptNum = sc.nextInt();
-                    sc.nextLine();
-                    System.out.println("name (Department): ");
-                    name = sc.nextLine();
-                    System.out.println("office (Department): ");
-                    office = sc.nextLine();
-                    createDepartment(deptNum, name, office);
-                    break;
-                case 5:
-                    System.out.println("deptNum (Department): ");
-                    deptNum = sc.nextInt();
-                    sc.nextLine();
-                    System.out.println("name (Department): ");
-                    name = sc.nextLine();
-                    System.out.println("office (Department): ");
-                    office = sc.nextLine();
+            try {
+                int op = Integer.parseInt(sc.nextLine());
+                if (op > 0 && op < 12) {
+                    int id;
+                    int deptNum, salary, per, years;
+                    String name, office;
+                    String namet, surnamet, emailt, d;
+                    Date startDatet;
 
-                    System.out.println("id (Teacher): ");
-                    id = sc.nextInt();
-                    sc.nextLine();
-                    System.out.println("name (Teacher): ");
-                    namet = sc.nextLine();
-                    System.out.println("surname (Teacher): ");
-                    surnamet = sc.nextLine();
-                    System.out.println("email (Teacher): ");
-                    emailt = sc.nextLine();
-                    System.out.println("start date (dd/MM/yyyy) (Teacher): ");
-                    d = sc.nextLine();
-                    startDatet = new SimpleDateFormat("dd/MM/yyyy").parse(d);
-                    System.out.println("salary (Teacher): ");
-                    salary = sc.nextInt();
-                    createTeacherAndDepartment(deptNum, name, office, id, namet, surnamet, emailt, startDatet, salary);
-                    break;
-                case 6:
-                    System.out.println("deptNum (Department): ");
-                    deptNum = sc.nextInt();
-                    sc.nextLine();
-                    System.out.println("id (Teacher): ");
-                    id = sc.nextInt();
-                    sc.nextLine();
-                    System.out.println("name (Teacher): ");
-                    namet = sc.nextLine();
-                    System.out.println("surname (Teacher): ");
-                    surnamet = sc.nextLine();
-                    System.out.println("email (Teacher): ");
-                    emailt = sc.nextLine();
-                    System.out.println("start date (dd/MM/yyyy) (Teacher): ");
-                    d = sc.nextLine();
-                    startDatet = new SimpleDateFormat("dd/MM/yyyy").parse(d);
-                    System.out.println("salary (Teacher): ");
-                    salary = sc.nextInt();
+                    switch (op) {
+                        case 1:
+                            System.out.println("ID (Department): ");
+                            id = sc.nextInt();
+                            showDepartment(id);
+                            break;
+                        case 2:
+                            System.out.println("ID (Teacher): ");
+                            id = sc.nextInt();
+                            showTeacher(id);
+                            break;
+                        case 3:
+                            System.out.println("ID (Department): ");
+                            id = sc.nextInt();
+                            showTeachersInDepartment(id);
+                            break;
+                        case 4:
+                            System.out.println("deptNum (Department): ");
+                            deptNum = sc.nextInt();
+                            sc.nextLine();
+                            System.out.println("name (Department): ");
+                            name = sc.nextLine();
+                            System.out.println("office (Department): ");
+                            office = sc.nextLine();
+                            createDepartment(deptNum, name, office);
+                            break;
+                        case 5:
+                            System.out.println("deptNum (Department): ");
+                            deptNum = sc.nextInt();
+                            sc.nextLine();
+                            System.out.println("name (Department): ");
+                            name = sc.nextLine();
+                            System.out.println("office (Department): ");
+                            office = sc.nextLine();
 
-                    createTeacherInExistingDepartment(deptNum, id, namet, surnamet, emailt, startDatet, salary);
-                    break;
-                case 7:
-                    System.out.println("id (Teacher): ");
-                    id = sc.nextInt();
-                    sc.nextLine();
-                    deleteTeacher(id);
-                    break;
-                case 8:
-                    System.out.println("deptNum (Department): ");
-                    deptNum = sc.nextInt();
-                    sc.nextLine();
-                    deleteDepartment(deptNum);
-                    break;
-                case 9:
-                    System.out.println("deptNum (Department): ");
-                    deptNum = sc.nextInt();
-                    sc.nextLine();
-                    System.out.println("salary (Of all teachers of the Department " + deptNum + "): ");
-                    salary = sc.nextInt();
-                    sc.nextLine();
-                    setSalaryOfDepartment(deptNum, salary);
-                    break;
-                case 10:
-                    System.out.println("deptNum (Department): ");
-                    deptNum = sc.nextInt();
-                    sc.nextLine();
-                    System.out.println("Percentage to raise salary (Of Seniors teachers of the Department " + deptNum + "): ");
-                    float perf = sc.nextFloat();
-                    sc.nextLine();
-                    System.out.println("Years to be considered Senior: ");
-                    years = sc.nextInt();
-                    sc.nextLine();
-                    riseSalaryOfDepartmentSeniors(deptNum, perf, years);
-                    break;
-                case 11:
-                    key = false;
-                    sessionFactory.close();
-                    break;
+                            System.out.println("id (Teacher): ");
+                            id = sc.nextInt();
+                            sc.nextLine();
+                            System.out.println("name (Teacher): ");
+                            namet = sc.nextLine();
+                            System.out.println("surname (Teacher): ");
+                            surnamet = sc.nextLine();
+                            System.out.println("email (Teacher): ");
+                            emailt = sc.nextLine();
+                            System.out.println("start date (dd/MM/yyyy) (Teacher): ");
+                            d = sc.nextLine();
+                            startDatet = new SimpleDateFormat("dd/MM/yyyy").parse(d);
+                            System.out.println("salary (Teacher): ");
+                            salary = sc.nextInt();
+                            createTeacherAndDepartment(deptNum, name, office, id, namet, surnamet, emailt, startDatet, salary);
+                            break;
+                        case 6:
+                            System.out.println("deptNum (Department): ");
+                            deptNum = sc.nextInt();
+                            sc.nextLine();
+                            System.out.println("id (Teacher): ");
+                            id = sc.nextInt();
+                            sc.nextLine();
+                            System.out.println("name (Teacher): ");
+                            namet = sc.nextLine();
+                            System.out.println("surname (Teacher): ");
+                            surnamet = sc.nextLine();
+                            System.out.println("email (Teacher): ");
+                            emailt = sc.nextLine();
+                            System.out.println("start date (dd/MM/yyyy) (Teacher): ");
+                            d = sc.nextLine();
+                            startDatet = new SimpleDateFormat("dd/MM/yyyy").parse(d);
+                            System.out.println("salary (Teacher): ");
+                            salary = sc.nextInt();
+
+                            createTeacherInExistingDepartment(deptNum, id, namet, surnamet, emailt, startDatet, salary);
+                            break;
+                        case 7:
+                            System.out.println("id (Teacher): ");
+                            id = sc.nextInt();
+                            sc.nextLine();
+                            deleteTeacher(id);
+                            break;
+                        case 8:
+                            System.out.println("deptNum (Department): ");
+                            deptNum = sc.nextInt();
+                            sc.nextLine();
+                            deleteDepartment(deptNum);
+                            break;
+                        case 9:
+                            System.out.println("deptNum (Department): ");
+                            deptNum = sc.nextInt();
+                            sc.nextLine();
+                            System.out.println("salary (Of all teachers of the Department " + deptNum + "): ");
+                            salary = sc.nextInt();
+                            sc.nextLine();
+                            setSalaryOfDepartment(deptNum, salary);
+                            break;
+                        case 10:
+                            System.out.println("deptNum (Department): ");
+                            deptNum = sc.nextInt();
+                            sc.nextLine();
+                            System.out.println("Percentage to raise salary (Of Seniors teachers of the Department " + deptNum + "): ");
+                            float perf = sc.nextFloat();
+                            sc.nextLine();
+                            System.out.println("Years to be considered Senior: ");
+                            years = sc.nextInt();
+                            sc.nextLine();
+                            riseSalaryOfDepartmentSeniors(deptNum, perf, years);
+                            break;
+                        case 11:
+                            key = false;
+                            sessionFactory.close();
+                            break;
+                    }
+                } else {
+                    System.out.println("You have to enter a number from 1 to 11");
+                }
+            } catch (NumberFormatException ex) {
+                System.out.println("You have to enter a number from 1 to 11");
             }
+        }while (key); 
+    }
+
+    private static boolean isNumeric(String cad) {
+        try {
+            Integer.parseInt(cad);
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
         }
     }
 }
