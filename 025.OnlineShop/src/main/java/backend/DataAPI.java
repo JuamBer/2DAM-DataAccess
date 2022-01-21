73a9669ccb625684168cde42652edf7941a5b698
Juan Sáez García <contacto@juamber.com>
2022-01-21 20:55:38 +0100

    api

--- 025.OnlineShop/src/main/java/backend/DataAPI.java
+++ 025.OnlineShop/src/main/java/backend/DataAPI.java
@@ -1,144 +1,202 @@
 package backend;
+
+//Mongo
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientSettings;
-import com.mongodb.client.FindIterable;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import static com.mongodb.client.model.Filters.eq;
+import com.mongodb.client.*;
+import static com.mongodb.client.model.Sorts.*;
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Updates.*;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+
+//Pojos Utilities
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
-import org.bson.conversions.Bson;
-import org.bson.types.ObjectId;
+
+//Pojos
 import pojos.Address;
 import pojos.Article;
 import pojos.Comment;
 import pojos.User;
+
+//Pojos Exceptions
 import pojos_exceptions.IncorrectArticleException;
 
+//Java
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class DataAPI {
-    
+
     private static MongoClient client;
     private static MongoDatabase db;
     private static String dbName;
-    
-    public DataAPI(){
+
+    public DataAPI() {
         DataAPI.dbName = "act5_3";
     }
-    public DataAPI(String dbName){
+
+    public DataAPI(String dbName) {
         DataAPI.dbName = dbName;
     }
-    
-    public static void init(){
+
+    public static void init() {
         DataAPI.client = new MongoClient();
         DataAPI.db = DataAPI.client.getDatabase(DataAPI.dbName);
-        
-        CodecRegistry pojoCodecRegistry = 
-                fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),                
-                fromProviders(PojoCodecProvider.builder().automatic(true).build()));
-        
+
+        CodecRegistry pojoCodecRegistry
+                = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
+                        fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+
         DataAPI.db = DataAPI.db.withCodecRegistry(pojoCodecRegistry);
     }
-    public static void close(){
+
+    public static void close() {
         DataAPI.client.close();
     }
-    
-    public static void insertArticle(Article art) throws IncorrectArticleException{
+
+    public static void insertArticle(Article art) throws IncorrectArticleException {
         MongoCollection<Article> articles = DataAPI.db.getCollection("articles", Article.class);
         MongoCollection<User> users = DataAPI.db.getCollection("users", User.class);
-        
-        System.out.println(art.getComments().size());
-        
-        
-        
+        articles.insertOne(art);
+
     }
-    
-    public static void insertUser(User us){
+
+    public static void insertUser(User us) {
         MongoCollection<User> users = DataAPI.db.getCollection("users", User.class);
         users.insertOne(us);
     }
-    
-    public static Article findArticle(ObjectId id){
+
+    public static Article findArticle(ObjectId id) {
         MongoCollection<Article> articles = DataAPI.db.getCollection("articles", Article.class);
-        Bson filter = eq("_id",id);
+        Bson filter = eq("_id", id.toString());
         Article articleRes = (Article) articles.find(filter);
         return articleRes;
     }
+
     /**
-        Returns all the articles that are of the category cat.
-    */
-    public static FindIterable<Article> findArticleByCategory(String cat){
-        return null;
+     * Returns all the articles that are of the category cat.
+     */
+    public static FindIterable<Article> findArticleByCategory(String cat) {
+        MongoCollection<Article> articles = DataAPI.db.getCollection("articles", Article.class);
+        Bson filter = eq("categories", cat);
+        FindIterable<Article> articlesResult = articles.find(filter);
+        return articlesResult;
     }
+
     /**
-       Returns all the articles that contains str in its name.
-    */
-    public static FindIterable<Article> findArticleByName(String str){
-        return null;
+     * Returns all the articles that contains str in its name.
+     */
+    public static FindIterable<Article> findArticleByName(String str) {
+        MongoCollection<Article> articles = DataAPI.db.getCollection("articles", Article.class);
+        Pattern strRegex = Pattern.compile("/i", Pattern.CASE_INSENSITIVE);
+        Bson filter = eq("name", strRegex);
+        FindIterable<Article> articlesResult = articles.find(filter);
+        return articlesResult;
     }
+
     /**
-       Returns all the articles whose price is in the rank [low, high], both inclusive.
-    */
-    public static FindIterable<Article> findArticleInPriceRank(double low, double high){
-        return null;
+     * Returns all the articles whose price is in the rank [low, high], both
+     * inclusive.
+     */
+    public static FindIterable<Article> findArticleInPriceRank(double low, double high) {
+        MongoCollection<Article> articles = DataAPI.db.getCollection("articles", Article.class);
+        Bson filter = and(gte("price", low), lte("price", high));
+        FindIterable<Article> articlesResult = articles.find(filter);
+        return articlesResult;
     }
-    public static User findUser(ObjectId id){
-        return null;
-    }
-    /**
-       Returns all the user who live in the country specified as argument.
-    */
-    public static FindIterable<User> findUserByCountry(String country){
+
+    public static User findUser(ObjectId id) {
         MongoCollection<User> users = DataAPI.db.getCollection("users", User.class);
-        Bson filter = eq("address.country",country);
+        Bson filter = eq("_id", id.toString());
+        User userRes = (User) users.find(filter);
+        return userRes;
+    }
+
+    /**
+     * Returns all the user who live in the country specified as argument.
+     */
+    public static FindIterable<User> findUserByCountry(String country) {
+        MongoCollection<User> users = DataAPI.db.getCollection("users", User.class);
+        Bson filter = eq("address.country", country);
         FindIterable<User> usersRes = (FindIterable<User>) users.find(filter);
         return usersRes;
     }
+
     /**
-       Receives a FindIterable<Article> object and returns it ordered by price ascending or descending as specified as argument.
-    */
-    public static FindIterable<Article> orderByPrice(FindIterable<Article> arts, boolean asc){
-        return null;
-        
+     * Receives a FindIterable<Article> object and returns it ordered by price
+     * ascending or descending as specified as argument.
+     */
+    public static FindIterable<Article> orderByPrice(FindIterable<Article> arts, boolean asc) {
+        FindIterable<Article> artsOrdered;
+        if (asc) {
+            artsOrdered = arts.sort(ascending("price"));
+        } else {
+            artsOrdered = arts.sort(descending("price"));
+        }
+        return artsOrdered;
     }
-    public static void updateAddress (User us, Address ad){
-        
+
+    public static void updateAddress(User us, Address ad) {
+        MongoCollection<User> users = DataAPI.db.getCollection("users", User.class);
+        Bson filter = eq("_id", us.getId());
+        Bson update = set("address",ad);
+        users.updateOne(filter,update); 
     }
-    public static void updateEmail(User us, String email){
+
+    public static void updateEmail(User us, String email) {
+        MongoCollection<User> users = DataAPI.db.getCollection("users", User.class);
+        Bson filter = eq("_id", us.getId());
+        Bson update = set("email",email);
+        users.updateOne(filter,update);
     }
-    public static void addComment(Article art, Comment newCom){
-        
-       // boolean usersExists = true;
-        //for (int i = 0; i < art.getComments().size(); i++) {
-        //    Bson filter = eq("_id",art.getComments().get(i).getId_user());
-        //    //FindIterable<User> res = users.find(filter);
-        //    System.out.println(res);
-        //    if(res == null){
-        //        usersExists=false;
-        //        continue;
-        //    }
-        //}
-        
-        //if(usersExists){
-        //    comm.insertOne(art);
-        //}else{
-        //    throw new IncorrectArticleException("At least one user of the article comments does not exist. Make sure they exist in the database");
-        //}
+
+    public static void addComment(Article art, Comment newCom) {
+        MongoCollection<Article> articles = DataAPI.db.getCollection("articles", Article.class);
+        Bson filter = eq("_id",art.getId());
+        Bson update = push("comments",newCom);
+        articles.updateOne(filter, update);
     }
+
     /**
-       Relete an article.
-    */
-    public static void deleteArticle(Article art){
+     * Delete an article.
+     */
+    public static void deleteArticle(Article art) {
         System.out.println("\n\n---deleteArticle---");
         MongoCollection<Article> articles = DataAPI.db.getCollection("users", Article.class);
-        Bson filter = eq("_id",art.getId());
+        Bson filter = eq("_id", art.getId());
         articles.deleteOne(filter);
     }
+
     /**
-       Relete a user and also all the comments whose author is the user.
-    */
-    public static void deleteUser(User us){
+     * Delete a user and also all the comments whose author is the user.
+     */
+    public static void deleteUser(User us) {
+        System.out.println("\n\n---deleteUser---");
+        MongoCollection<Article> articles = DataAPI.db.getCollection("articles", Article.class);
+        MongoCollection<User> users = DataAPI.db.getCollection("users", User.class);
+        
+        
+        Bson filter = eq("comments.id_user", us.getId());
+        Bson update = null;
+        articles.updateMany(filter,update);
+        FindIterable<Article> articlesRes = articles.find(filter);
+        
+        
+        for (Article article : articlesRes) {
+            List<Comment> comments = article.getComments();
+            for (int i = 0; i < comments.size(); i++) {
+                if (comments.get(i).getId_user() == us.getId()) {
+                    comments.remove(i);
+                }
+            }
+        }
+        Bson filterUser = eq("_id", us.getId());
+        users.deleteOne(filterUser);
     }
 
 }
--- 025.OnlineShop/src/main/java/onlineshop/OnlineShop.java
+++ 025.OnlineShop/src/main/java/onlineshop/OnlineShop.java
@@ -1,7 +1,14 @@
 package onlineshop;
 
 import backend.DataAPI;
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
+import org.bson.Document;
 import pojos.Address;
 import pojos.Article;
 import pojos.Comment;
@@ -9,59 +16,190 @@ import pojos.User;
 import pojos_exceptions.IncorrectArticleException;
 
 public class OnlineShop {
-    public static void main(String[] args) throws IncorrectArticleException {
-        DataAPI api = new DataAPI();
+    
+    private static DataAPI api;
+    
+    public static void main(String[] args) throws IncorrectArticleException, IOException {
+        OnlineShop.dropDatabase();
+        OnlineShop.insertDemoData();
         
-        //Method Init
+        api = new DataAPI();
         api.init();
         
-        //Method Insert Article
-        Address addressUser1 = new Address(
-                "Plaza Musico Ramon Ibars",
-                5,
-                "Valencia",
-                "España"
-        );
-        Address addressUser2 = new Address(
-                "Travessera Cuellar	",
-                328,
-                "Aragón Alta",
-                "España"
-        );
+        System.out.println("insertArticle");
+        Article newArticle = new Article("Test Article Title",95.0,Arrays.asList("CategoryTest1","CategoryTest2"));
+        Article newArticle2 = new Article("Other Test Article Title",300.60,Arrays.asList("CategoryTest1","CategoryTest2"));
+        api.insertArticle(newArticle);
+        api.insertArticle(newArticle2);
         
-        User user1 = new User(
-                "Juan Sáez García",
-                "contacto@juamber.com",
-                addressUser1
-        );
-        User user2 = new User(
-                "Ivan Soriano",
-                "contacto@ivanzon.com",
-                addressUser2
-        );
-        Comment comment1 = new Comment(
-                4,
-                "Camera result is really good and overall a nice all rounder phone. Loving it!",
-                user1.getId()
-        );
-        Comment comment2 = new Comment(
-                2,
-                "Demasiado caro",
-                user2.getId()
-        );
+        System.out.println("insertUser");
+        User newUser = new User("Test User Name","email@test.com",new Address("Test Direcction",1,"TestCity","TextCountry"));
+        api.insertUser(newUser);
         
-        Article article = new Article(
-                "Google Pixel 6 Pro",
-                1.180,
-                Arrays.asList("Smatphone","Tech","Google")
-        );
-        //api.insertUser(user1);
-        //api.insertUser(user2);
+        //System.out.println("findArticle");
+        //Article recoveredArticle = api.findArticle(newArticle.getId());
+        //System.out.println("\t"+recoveredArticle);
         
-        //api.insertArticle(article);
+        System.out.println("findArticleByCategory");
+        FindIterable<Article> articlesByCategory = api.findArticleByCategory("Smartphone");
+        for (Article article : articlesByCategory) {
+            System.out.println("\t"+article);
+        }
         
+        System.out.println("findArticleByName");
+        FindIterable<Article> articlesByName = api.findArticleByName("oo");
+        for (Article article : articlesByName) {
+            System.out.println("\t"+article);
+        }
         
+        System.out.println("findArticleInPriceRank");
+        FindIterable<Article> articlesByPriceRank = api.findArticleInPriceRank(500,1500);
+        for (Article article : articlesByPriceRank) {
+            System.out.println("\t"+article);
+        }
+        
+        //System.out.println("findUser");
+        //User recoveredUser = api.findUser(newUser.getId());
+        //System.out.println("\t"+recoveredUser);
+        
+        System.out.println("findUserByCountry");
+        FindIterable<User> usersByCountry = api.findUserByCountry("España");
+        for (User user : usersByCountry) {
+            System.out.println("\t"+user);
+        }
+        
+        System.out.println("orderByPrice");
+        FindIterable<Article> ArticlesOrderedByPrice = api.orderByPrice(articlesByName,true);
+        for (Article article : ArticlesOrderedByPrice) {
+            System.out.println("\t"+article);
+        }
+        
+        System.out.println("updateAddress");
+        api.updateAddress(newUser,new Address("Update Address!",1,"TestCity","TextCountry"));
+        
+        System.out.println("updateEmail");
+        api.updateEmail(newUser,"updated@email.com");
+        
+        System.out.println("addComment");
+        Comment newComment = new Comment(4,"New Comment !", newUser.getId());
+        api.addComment(newArticle2,newComment);
+        
+        System.out.println("deleteArticle");
+        api.deleteArticle(newArticle);
+        
+        System.out.println("deleteUser");
         
         api.close();
+    }
+    
+    public static void printFindIterableItems(FindIterable<Object> fi){
+        
+  
+        
+    }
+    public static void testInsertUser(){
+        
+    }
+    public static void testFindArticle(){
+        
+    }
+    
+    public static void insertDemoData() throws IOException{
+        
+        MongoClient client = new MongoClient();
+
+        MongoDatabase database = client.getDatabase("act5_3");
+        MongoCollection<Document> users = database.getCollection("users");
+        MongoCollection<Document> articles = database.getCollection("articles");
+        
+        Document user1 = Document.parse("{\n" +
+        "        \"email\": \"contacto@juamber.com\",\n" +
+        "        \"name\": \"Juan Sáez García\",\n" +
+        "        \"address\": {\n" +
+        "            \"city\": \"Valencia\",\n" +
+        "            \"country\": \"España\",\n" +
+        "            \"number\": 5,\n" +
+        "            \"street\": \"Plaza Musico Ramon Ibars\"\n" +
+        "        }\n" +
+        "    }");
+        Document user2 = Document.parse("{\n" +
+        "        \"email\": \"franciscojavier.gamez@salgado.es\",\n" +
+        "        \"name\": \"Francisco Javier\",\n" +
+        "        \"address\": {\n" +
+        "            \"city\": \"Madrid\",\n" +
+        "            \"country\": \"España\",\n" +
+        "            \"number\": 8,\n" +
+        "            \"street\": \"Paseo Galindo\"\n" +
+        "        }\n" +
+        "    }");
+        Document user3 = Document.parse("{\n" +
+        "        \"email\": \"corlina.velaz2@gmail.com\",\n" +
+        "        \"name\": \"Carolina Velázquez Segundo\",\n" +
+        "        \"address\": {\n" +
+        "            \"city\": \"Valencia\",\n" +
+        "            \"country\": \"España\",\n" +
+        "            \"number\": 92,\n" +
+        "            \"street\": \"Ronda Jordi\"\n" +
+        "        }\n" +
+        "    }");
+        Document user4 = Document.parse("{\n" +
+        "        \"email\": \"fat_palacios@hotmail.com\",\n" +
+        "        \"name\": \"Fátima Palacios\",\n" +
+        "        \"address\": {\n" +
+        "            \"city\": \"Barcelona\",\n" +
+        "            \"country\": \"España\",\n" +
+        "            \"number\": 64,\n" +
+        "            \"street\": \"Carrer Vega\"\n" +
+        "        }\n" +
+        "    }");
+        Document user5 = Document.parse("{\n" +
+        "        \"email\": \"cristian45@vega.com\",\n" +
+        "        \"name\": \"Cristian García\",\n" +
+        "        \"address\": {\n" +
+        "            \"city\": \"Touluse\",\n" +
+        "            \"country\": \"Francia\",\n" +
+        "            \"number\": 23,\n" +
+        "            \"street\": \"Avenida Ariadna\"\n" +
+        "        }\n" +
+        "    }");
+        Document user6 = Document.parse("{\n" +
+        "        \"email\": \"carla59@live.com\",\n" +
+        "        \"name\": \"Carla Delao\",\n" +
+        "        \"address\": {\n" +
+        "            \"city\": \"Madrid\",\n" +
+        "            \"country\": \"España\",\n" +
+        "            \"number\": 3,\n" +
+        "            \"street\": \"Travessera Raquel\"\n" +
+        "        }\n" +
+        "    }");
+        
+        users.insertMany(Arrays.asList(user1,user2,user3,user4,user5,user6));
+        
+        Document article1 = Document.parse("{\n" +
+        "        \"name\": \"Google Pixel 6 Pro\",\n" +
+        "        \"price\": 1180.23,\n" +
+        "        \"categories\": [\"Smartphone\", \"Electronic\", \"Google\"]\n" +
+        "    }");
+        Document article2 = Document.parse("{\n" +
+        "        \"name\": \"MacBook Pro 13' 2020\",\n" +
+        "        \"price\": 1602.62,\n" +
+        "        \"categories\": [\"Laptop\", \"Work\" ,\"Electronic\", \"Apple\"]\n" +
+        "    }");
+        Document article3 = Document.parse("{\n" +
+        "        \"name\": \"Iphone 11 128GB\",\n" +
+        "        \"price\": 553.0,\n" +
+        "        \"categories\": [\"Smartphone\", \"Electronic\", \"Apple\"]\n" +
+        "    }");
+        Document article4 = Document.parse("{\n" +
+        "        \"name\": \"Sony WH1000XM3\",\n" +
+        "        \"price\": 221.96,\n" +
+        "        \"categories\": [\"Audio\", \"Professional\", \"Electronic\", \"Sony\"]\n" +
+        "    }");
+        
+        articles.insertMany(Arrays.asList(article1,article2,article3,article4));
+    }
+    public static void dropDatabase(){
+        MongoClient client = new MongoClient();
+        client.getDatabase("act5_3").drop();
     }
 }
